cd9e93c0fc6c2a288339bc973bf828094a0518a4
mvaude <maxime.vaude@scality.com>
2017-02-01 15:34:13 +0100

    feature(core): add basic report system
    - add cmd section
    - add coverage file and picsou binary to gitignore
    - add main email skeleton
    - improve credentials management using environment variables (not
      perfect solution)

---
+++ pkg/report/email.go
@@ -0,0 +1,84 @@
+package report
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/scality/picsou/pkg/stats"
+	"html/template"
+	"net/smtp"
+	"time"
+)
+
+// TODO: 3 different example report levels: total, regions, person (SSH KEY)
+
+// TemplateData struct
+type TemplateData struct {
+	Name  string
+	Stats *stats.Stats
+}
+
+// Template struct
+type Template struct {
+	filename string
+	data     *TemplateData
+}
+
+// Request struct
+type Request struct {
+	auth     *smtp.Auth
+	from     string
+	to       []string
+	subject  string
+	body     string
+	template *Template
+}
+
+// NewRequest create a new Request struct with the parameters given
+func NewRequest(auth *smtp.Auth, to []string, subject, body, templatePath string, templateData *TemplateData) *Request {
+	return &Request{
+		auth:    auth,
+		to:      to,
+		subject: subject,
+		body:    body,
+		template: &Template{
+			filename: templatePath,
+			data:     templateData,
+		},
+	}
+}
+
+// SendEmail handles sending an email with the formated template
+func (r *Request) SendEmail() *Request {
+	date := time.Now()
+	year, month, day := date.Date()
+	hour, min, sec := date.Clock()
+	now := fmt.Sprintf("%02d/%02d/%d %02d:%02d:%02d", day, month, year, hour, min, sec)
+	from := "From: maxime.vaude@gmail.com\r\n"
+	to := "To: " + r.to[0] + "\r\n"
+	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\r\n"
+	subject := fmt.Sprintf("Subject: %s %s\r\n\r\n", r.subject, now)
+	msg := []byte(from + to + mime + subject + "\n" + r.body + "\r\n")
+	addr := "smtp.gmail.com:587"
+
+	if err := smtp.SendMail(addr, *r.auth, "maxime.vaude@gmail.com", r.to, msg); err != nil {
+		fmt.Println("pb during SendMail: ", err)
+		return nil
+	}
+	return r
+}
+
+// ParseTemplate format the template using the metadata giver by the struct data
+func (r *Request) ParseTemplate() *Request {
+	t, err := template.ParseFiles(r.template.filename)
+	if err != nil {
+		fmt.Println("pb during parse template: ", err)
+		return nil
+	}
+	buf := new(bytes.Buffer)
+	if err = t.Execute(buf, r.template.data); err != nil {
+		fmt.Println("pb during execute template: ", err)
+		return nil
+	}
+	r.body = buf.String()
+	return r
+}
--- pkg/stats/ec2.go
+++ pkg/stats/ec2.go
@@ -2,17 +2,17 @@ package stats
 
 import (
 	"fmt"
-	"sync"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudwatch"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
 )
 
 // Regions defines all amazon regions to check for ec2 usage
-var Regions = []string{
+var regions = []string{
 	"us-east-1",
 	"us-east-2",
 	"us-west-1",
@@ -49,28 +49,54 @@ type Instance struct {
 // http://docs.aws.amazon.com/general/latest/gr/glos-chap.html#reservation
 type Reservation struct {
 	// Instances started
-	Instances []Instance
+	CloudWatch       *CloudWatch
+	Instances        []*ec2.Instance
+	InstancesRunning []int
+	InstancesUsage   float64
+	Volumes          []*ec2.Volume
+	VolumesUsage     float64
+	// VolumesTotalSize int
+	// VolumesUsedSize int
 }
 
-// EC2 represente a collection of Regions and reservations from aws-ec2
+// User structure keeping some important user metadata:
+//   - Reservations: reservations for this user
+//   - Volumes: volumes for this user
+type User struct {
+	Reservations map[int][]int
+	Volumes      []int
+}
+
+// EC2 represents a collection of Regions and reservations from aws-ec2
 //
 // For more documentation see:
 // https://aws.amazon.com/ec2/
 type EC2 struct {
-	// Array of region of the reservations
-	Regions []string
+	Client *ec2.EC2
 	// Array of reservations for the ec2 account
-	Reservations []Reservation
+	CloudWatch          *CloudWatch
+	Reservations        []*Reservation
+	ReservationsRunning []int
+	ReservationsUsage   float64
+	Users               map[string]*User
+	Volumes             []*ec2.Volume
+	VolumesUsage        float64
 }
 
-// Stats represente the differents statistics for the differents reservations
+// Stats represents the differents statistics for the differents reservations
 // and instances from the ec2 account
 type Stats struct {
 	// Differents reservations and instances
-	Service EC2
+	Service map[string]*EC2
+	Regions []string
 }
 
-// GetState match the status code with the representing status string
+// CloudWatch structure
+type CloudWatch struct {
+	Client *cloudwatch.CloudWatch
+}
+
+// GetState matches the status code with the representing status string
 func GetState(code int64) string {
 	switch code {
 	case 0:
@@ -91,52 +117,71 @@ func GetState(code int64) string {
 
 // New will get from aws all the reservations and then the instances
 // from that informations the function will compute the appropriate stats
-func New(sess *session.Session, regions []string) *Stats {
-	var wg sync.WaitGroup
-	nums := make(chan int)
-	srv := &Stats{EC2{Regions: regions}}
-	for i := range srv.Service.Regions {
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
-			region := srv.Service.Regions[i]
-			svc := ec2.New(sess, &aws.Config{Region: aws.String(region)})
-			nm := srv.listInstances(svc)
-			fmt.Println("Region Name: ", region, " | Number of instances: ", nm)
-			nums <- nm
-		}(i)
+func New(sess *session.Session) *Stats {
+	srv := &Stats{Regions: regions, Service: make(map[string]*EC2)}
+	for _, region := range regions {
+		newService := &EC2{
+			Client:       ec2.New(sess, &aws.Config{Region: aws.String(region)}),
+			CloudWatch:   &CloudWatch{Client: cloudwatch.New(sess, &aws.Config{Region: aws.String(region)})},
+			Reservations: []*Reservation{},
+			Volumes:      []*ec2.Volume{},
+			Users:        make(map[string]*User),
+		}
+		newService = newService.getReservations(newService.Client).getRunningInstances().getReservationsUsage().getVolumes().getVolumesUsage().getUsers().getUsages()
+		if len(newService.Reservations) != 0 {
+			srv.Service[region] = newService
+		}
 	}
-	go func() {
-		wg.Wait()
-		close(nums)
-	}()
-	sum := 0
-	for num := range nums {
-		sum += num
-	}
-	fmt.Println("done\ntotal instances: ", sum)
 	return srv
 }
 
-func (s Stats) getInstances(reservation int, instances []*ec2.Instance) {
-	// fmt.Println("reservation: ", reservation, " instances: ", len(instances))
-	t := time.Now()
-	for _, instance := range instances {
-		fmt.Print("id: ", *instance.InstanceId)
-		fmt.Print(" | deltatime: ", t.Sub(*instance.LaunchTime))
-		// fmt.Print(
-		// 	" | LaunchTime: ",
-		// 	instance.LaunchTime.Hour(),
-		// 	":",
-		// 	instance.LaunchTime.Minute(),
-		// )
-		// fmt.Print(" | ClientToken: ", *instance.ClientToken)
-		// fmt.Println(" | KeyName: ", *instance.KeyName)
-		// fmt.Println(" | State: ", s.GetState(*instance.State.Code))
+func (s *EC2) getUsages() *EC2 {
+	for _, r := range s.Reservations {
+		r.getInstancesUsage()
 	}
+	return s
 }
 
-func (s Stats) listInstances(svc ec2iface.EC2API) int {
+// RunningInstances returns a indexes' list of instances running in the instances list given.
+func (s *EC2) RunningInstances(instances []*ec2.Instance) []int {
+	var running []int
+	for i, instance := range instances {
+		if GetState(*instance.State.Code) == "running" {
+			running = append(running, i)
+		}
+	}
+	return running
+}
+
+func (s *EC2) getRunningInstances() *EC2 {
+	var reservationsRunning []int
+	for i, reservation := range s.Reservations {
+		if len(reservation.InstancesRunning) != 0 {
+			reservationsRunning = append(reservationsRunning, i)
+		}
+	}
+	s.ReservationsRunning = reservationsRunning
+	return s
+}
+
+// GetRunningRatio returns the reservations running percentage in a region.
+func (s *EC2) GetRunningRatio() float64 {
+	if len(s.Reservations) == 0 {
+		return float64(0)
+	}
+	return float64(len(s.ReservationsRunning)) / float64(len(s.Reservations)) * 100.0
+}
+
+// GetInstancesRatio returns the instances running percentage in a reservation
+func (s *Reservation) GetInstancesRatio() float64 {
+	if len(s.Instances) == 0 {
+		return float64(0)
+	}
+	return float64(len(s.InstancesRunning)) / float64(len(s.Instances)) * 100.0
+}
+
+func (s *EC2) getReservations(svc ec2iface.EC2API) *EC2 {
+	running := 0
 	count := 0
 
 	// Call the DescribeInstances Operation
@@ -146,15 +191,267 @@ func (s Stats) listInstances(svc ec2iface.EC2API) int {
 	}
 
 	// resp has all of the response data, pull out instance IDs:
-	reservation := 1
-	for _, res := range resp.Reservations {
-		//fmt.Println("\n\n** instances: ", res.Instances)
-		// fmt.Println(res)
-		// fmt.Print("Owner: ", *res.OwnerId)
-		// fmt.Println(" | ReservationId: ", *res.ReservationId)
-		// s.getInstances(reservation, res.Instances)
-		count += len(res.Instances)
-		reservation++
+	for _, reservation := range resp.Reservations {
+		instancesRunning := s.RunningInstances(reservation.Instances)
+		if len(reservation.Instances) != 0 {
+			s.Reservations = append(s.Reservations, &Reservation{Instances: reservation.Instances, InstancesRunning: instancesRunning, CloudWatch: s.CloudWatch})
+		} else {
+			s.Reservations = append(s.Reservations, &Reservation{Instances: reservation.Instances, InstancesRunning: instancesRunning, CloudWatch: s.CloudWatch})
+		}
+		running += len(instancesRunning)
+		count += len(reservation.Instances)
+	}
+	return s
+}
+
+// GetVolumesSize returns the total volumes size for the targeted volumes.
+// If no targets is declared, it will loop over all volumes in the specific region.
+func (s *EC2) GetVolumesSize(targets []int) int64 {
+	var count int64
+	if targets != nil {
+		for _, target := range targets {
+			count += *s.Volumes[target].Size
+		}
+	} else {
+		for _, volume := range s.Volumes {
+			count += *volume.Size
+		}
 	}
 	return count
+}
+
+func (s *EC2) getVolumes() *EC2 {
+	// var filterName = "availability-zone"
+	volumesOutput, err := s.Client.DescribeVolumes(nil)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	s.Volumes = append(s.Volumes, volumesOutput.Volumes...)
+	return s
+}
+
+// GetReservationsUsage returns the usage average of running reservations in a region
+func (s *EC2) GetReservationsUsage(targets []int) float64 {
+	var total, nb float64
+
+	startTime := time.Now().AddDate(0, 0, -1)
+	delta, err := time.ParseDuration("-10m")
+	if err != nil {
+		fmt.Println("error during time parsing: ", err)
+	}
+	endTime := time.Now().Add(delta)
+
+	for _, target := range targets {
+		for _, instance := range s.Reservations[target].Instances {
+			res, err := s.CloudWatch.Client.GetMetricStatistics(&cloudwatch.GetMetricStatisticsInput{
+				MetricName: aws.String("CPUUtilization"),
+				Namespace:  aws.String("AWS/EC2"),
+				Dimensions: []*cloudwatch.Dimension{
+					&cloudwatch.Dimension{
+						Name:  aws.String("InstanceId"),
+						Value: instance.InstanceId,
+					},
+				},
+				StartTime:  aws.Time(startTime),
+				EndTime:    aws.Time(endTime),
+				Period:     aws.Int64(3600),
+				Statistics: []*string{aws.String("Sum"), aws.String("SampleCount")},
+			})
+			if err != nil {
+				fmt.Println("error during cloudwatch getMetrics: ", err)
+			}
+			for _, datapoint := range res.Datapoints {
+				if *datapoint.Sum > 0.0 {
+					nb += *datapoint.SampleCount
+					total += *datapoint.Sum
+				}
+			}
+		}
+	}
+	if int(nb) != 0 {
+		return total / nb
+	}
+	return 0.0
+}
+
+func (s *EC2) getReservationsUsage() *EC2 {
+	s.ReservationsUsage = s.GetReservationsUsage(s.ReservationsRunning)
+	return s
+}
+
+func (s *EC2) getUsers() *EC2 {
+	var username string
+	users := make(map[string]*User)
+	for r, reservation := range s.Reservations {
+		for i, instance := range reservation.Instances {
+			username = *instance.KeyName
+			if _, ok := users[username]; !ok {
+				users[username] = &User{
+					Reservations: make(map[int][]int),
+					Volumes:      []int{},
+				}
+			}
+			users[username].Reservations[r] = append(users[username].Reservations[r], i)
+		}
+	}
+	for i, volume := range s.Volumes {
+		if len(volume.Attachments) != 0 {
+			for _, v := range volume.Attachments {
+				params := &ec2.DescribeInstancesInput{
+					Filters: []*ec2.Filter{
+						{
+							Name:   aws.String("instance-id"),
+							Values: []*string{v.InstanceId},
+						},
+					},
+				}
+				resp, err := s.Client.DescribeInstances(params)
+				if err != nil {
+					fmt.Println("error when describing instances for volume: ", err)
+				}
+				for _, res := range resp.Reservations {
+					for _, ins := range res.Instances {
+						username = *ins.KeyName
+						users[username].Volumes = append(users[username].Volumes, i)
+					}
+				}
+			}
+		}
+	}
+	s.Users = users
+	return s
+}
+
+// GetUsersRunning returns the running reservations for a user
+func (s *EC2) GetUsersRunning(user string) int {
+	count := 0
+	for r := range s.Users[user].Reservations {
+		for _, ri := range s.ReservationsRunning {
+			if r == ri {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+// Divide divide two floats and give a percentage
+func (s *Stats) Divide(a, b int) float64 {
+	if b == 0 {
+		return float64(0)
+	}
+	return float64(a) / float64(b) * 100
+}
+
+// ReservationsKeys get all reserveration indexes for the User's reservations
+func (s *User) ReservationsKeys() []int {
+	var keys []int
+	for key := range s.Reservations {
+		keys = append(keys, key)
+	}
+	return keys
+}
+
+// GetInstancesUsage return the usage average for the targeted volumes
+// If no targets are given, it will compute the average for all reservation instances
+func (s *Reservation) GetInstancesUsage(targets []int) float64 {
+	var instance *ec2.Instance
+	var total, nb float64
+
+	startTime := time.Now().AddDate(0, 0, -1)
+	delta, err := time.ParseDuration("-10m")
+	if err != nil {
+		fmt.Println("error during time parsing: ", err)
+	}
+	endTime := time.Now().Add(delta)
+
+	for _, target := range targets {
+		instance = s.Instances[target]
+		res, err := s.CloudWatch.Client.GetMetricStatistics(&cloudwatch.GetMetricStatisticsInput{
+			MetricName: aws.String("CPUUtilization"),
+			Namespace:  aws.String("AWS/EC2"),
+			Dimensions: []*cloudwatch.Dimension{
+				&cloudwatch.Dimension{
+					Name:  aws.String("InstanceId"),
+					Value: instance.InstanceId,
+				},
+			},
+			StartTime:  aws.Time(startTime),
+			EndTime:    aws.Time(endTime),
+			Period:     aws.Int64(3600),
+			Statistics: []*string{aws.String("Sum"), aws.String("SampleCount")},
+		})
+		if err != nil {
+			fmt.Println("error during cloudwatch getMetrics: ", err)
+		}
+		for _, datapoint := range res.Datapoints {
+			if *datapoint.Sum > 0.0 {
+				nb += *datapoint.SampleCount
+				total += *datapoint.Sum
+			}
+		}
+	}
+	if int(nb) != 0 {
+		return total / nb
+	}
+	return 0.0
+}
+
+func (s *Reservation) getInstancesUsage() *Reservation {
+	s.InstancesUsage = s.GetInstancesUsage(s.InstancesRunning)
+	return s
+}
+
+// GetVolumesUsage return the usage average for the targeted volumes
+// If no targets are given, it will compute the average for all region volumes
+func (s *EC2) GetVolumesUsage(targets []int) float64 {
+	var total, nb float64
+	startTime := time.Now().AddDate(0, 0, -1)
+	delta, err := time.ParseDuration("-10m")
+	if err != nil {
+		fmt.Println("error during time parsing: ", err)
+	}
+	endTime := time.Now().Add(delta)
+	var iterator []*ec2.Volume
+	if targets != nil {
+		for _, vol := range targets {
+			iterator = append(iterator, s.Volumes[vol])
+		}
+	} else {
+		iterator = s.Volumes
+	}
+	for _, volume := range iterator {
+		res, err := s.CloudWatch.Client.GetMetricStatistics(&cloudwatch.GetMetricStatisticsInput{
+			MetricName: aws.String("VolumeIdleTime"),
+			Namespace:  aws.String("AWS/EBS"),
+			Dimensions: []*cloudwatch.Dimension{
+				&cloudwatch.Dimension{
+					Name:  aws.String("VolumeId"),
+					Value: volume.VolumeId,
+				},
+			},
+			StartTime:  aws.Time(startTime),
+			EndTime:    aws.Time(endTime),
+			Period:     aws.Int64(3600),
+			Statistics: []*string{aws.String("Sum"), aws.String("SampleCount")},
+		})
+		if err != nil {
+			fmt.Println("error during cloudwatch getMetrics: ", err)
+		}
+		for _, datapoint := range res.Datapoints {
+			if *datapoint.Sum > 0.0 {
+				nb += *datapoint.SampleCount
+				total += *datapoint.Sum
+			}
+		}
+	}
+	if int(nb) != 0 {
+		return 100.0 - total/nb/3600.0*100
+	}
+	return 100.0
+}
+
+func (s *EC2) getVolumesUsage() *EC2 {
+	s.VolumesUsage = s.GetVolumesUsage(nil)
+	return s
 }
