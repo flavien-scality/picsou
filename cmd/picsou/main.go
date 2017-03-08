f9e04fadd4e41b9ef2424401baa56ee3483d18a7
Maxime Vaude <mvaude@users.noreply.github.com>
2017-03-08 11:02:37 +0100

    feature: add cli framework with basic skeleton (#21)

--- cmd/picsou/main.go
+++ cmd/picsou/main.go
@@ -5,13 +5,15 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/scality/picsou/pkg/report"
 	"github.com/scality/picsou/pkg/stats"
+	"github.com/urfave/cli"
 	"net/smtp"
 	"os"
+	"sort"
 )
 
 var auth smtp.Auth
 
-func main() {
+func getReport() {
 	auth := smtp.PlainAuth("", os.Getenv("PICSOU_USER"), os.Getenv("PICSOU_PSD"), "smtp.gmail.com")
 	sess, err := session.NewSession()
 	if err != nil {
@@ -26,4 +28,39 @@ func main() {
 	if r != nil {
 		fmt.Println("SendEmail Failure: ", err)
 	}
+}
+
+func main() {
+	app := cli.NewApp()
+
+	app.Commands = []cli.Command{
+		{
+			Name: "upload",
+			Aliases: []string{"u"},
+			Usage: "upload secret file to S3",
+			Action: func(c *cli.Context) error {
+				return nil
+			},
+		},
+		{
+			Name: "download",
+			Aliases: []string{"d"},
+			Usage: "download secret file from S3",
+			Action: func(c *cli.Context) error {
+				return nil
+			},
+		},
+		{
+			Name: "report",
+			Aliases: []string{"r"},
+			Usage: "send AWS costs report",
+			Action: func(c *cli.Context) error {
+				getReport()
+				return nil
+			},
+		},
+	}
+
+	sort.Sort(cli.CommandsByName(app.Commands))
+	app.Run(os.Args)
 }
