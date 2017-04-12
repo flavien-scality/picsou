c53a3d3df7f71ca41a65c5791bfa0ecdd2d27b2e
mvaude <maxime.vaude@scality.com>
2017-04-12 17:53:13 +0200

    add main skeleton for telegraf being set in the ami ubuntu xenial.

---
+++ use-cases/amis/ubuntu-xenial/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -x -e -u -o pipefail
+
+curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+source /etc/lsb-release
+echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get install telegraf
+sudo systemctl start telegraf
