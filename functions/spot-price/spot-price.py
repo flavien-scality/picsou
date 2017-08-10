cb9c67e168759fe3a127dc6cd70b2f3aecabfe3e
Maxime Vaude <maxime.vaude@gmail.com>
2017-08-10 17:36:57 +0200

    refactor spot-price:
    - refactor and add essential roles
    - improve algorithm to get best advise price

--- functions/spot-price/spot-price.py
+++ functions/spot-price/spot-price.py
@@ -3,17 +3,19 @@ Lambda to get best price for an ec2 instance filling specific conditions
 """
 
 import boto3
+import botocore
 
 from datetime import datetime, timedelta
 import json
 import logging
+import os
 import re
 import time
-import urllib2
+from urllib.request import urlopen, Request
 
 logging.basicConfig()
 logger = logging.getLogger()
-log_level = os.getenv("LOG_LEVEL", logging.INFO)
+log_level = os.getenv("LOG_LEVEL", logging.DEBUG)
 logger.setLevel(log_level)
 
 def datetime_handler(x):
@@ -34,24 +36,32 @@ def get_price(DryRun=True, event=None):
     :rtype: Iterable(float)
     """
     url = "https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/current/eu-west-2/index.json"
-    req = urllib2.Request(url)
-    opener = urllib2.build_opener()
-    f = opener.open(req)
-    prices = json.loads(f.read())
+    with urlopen(url) as res:
+        prices = json.loads(res.read().decode())
 
     client = boto3.client("ec2")
+    today = datetime.now()
     response = client.describe_spot_price_history(
         DryRun=DryRun,
-        StartTime=event.get("StartTime", ""),
-        EndTime=event.get("EndTime", ""),
+        StartTime=event.get("StartTime", today - timedelta(days=1)),
+        EndTime=event.get("EndTime", today),
         InstanceTypes=[ "|".join(event.get("InstanceTypes", "")) ],
         ProductDescriptions=event.get("ProductDescriptions", []),
-        Filters=event.get("Filters", []),
+        Filters=event.get("Filters", [
+            {
+                'Name': 'product-description',
+                'Values': [
+                    'Linux/UNIX',
+                ]
+            }
+        ]),
         AvailabilityZone=event.get("AvailabilityZone", ""),
         MaxResults=event.get("MaxResults", 1000),
         NextToken=event.get("NextToken", "")
     )
+    rprice = 0.0
     sprice = 1.0
+    result = 0.0
     for sprices in response["SpotPriceHistory"]:
         if float(sprices["SpotPrice"]) < sprice:
             sprice = float(sprices["SpotPrice"])
@@ -62,23 +72,28 @@ def get_price(DryRun=True, event=None):
                 # TODO: check for different tenancies (ex: dedicated)
                 if "Linux" in metadatas and "Host" not in metadatas and "On Demand" in metadatas:
                     instance = re.findall("\w+\.\w+", metadatas)
+                    logger.debug("get on demand price for {1}: {0}".format(instance[0], instance[1]))
                     price = float(instance[0])
+                    logger.debug(type(price))
                     instance_type = instance[1]
                     # instance_tenancy = re.findall("(On Demand|Dedicated)", metadatas)[0]
                     if any(instance_type in itype for itype in event.get("InstanceTypes", [])):
-                        result = sprice + 0.05 * price
-    return {
+                        result = sprice + 0.05 * price if sprice < price else price
+                        rprice = price
+    res = {
+        "onDemandPrice": rprice,
         "spotPrice": sprice,
-        "onDemandPrice": price,
-        "advisePrice": result
+        "advisePrice": result,
     }
+    logger.info("Get price for {}: {}".format(event.get("InstanceTypes", []), res))
+    return res
     # regex to get [price, instances type]: ([^\s]+\.[^\s]+)
     # regex to get tenancy: (On Demand|Dedicated)
     # print("\n\nPrices: {}".format(json.dumps(prices["terms"], indent=4)))
     # print("\n\nPrices: {}".format(prices["terms"]["OnDemand"].keys()))
 
 
-def handle(event, context):
+def handler(event, context):
     """
     Lambda handler
     """
@@ -98,8 +113,7 @@ if __name__ == "__main__":
             }
         ],
         "StartTime": today - timedelta(days=1),
-        "StartTime": today,
         "EndTime": today,
         "InstanceTypes": [ "c4.xlarge" ],
     }
-    get_price(DryRun=False, event=event)
+    print(get_price(DryRun=False, event=event))
