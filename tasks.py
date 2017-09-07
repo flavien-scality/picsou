900be11914052d6b2d86b39be7ee9a5a1246d0dc
mvaude <maxime.vaude@scality.com>
2017-09-07 14:54:00 +0200

    improve terraform deploy logic (DryRun for the moment)

--- tasks.py
+++ tasks.py
@@ -9,14 +9,25 @@ import os
 def deploy_lambdas(ctx):
   output = ctx.run("ls functions")
   for lambda_name in output.stdout.split("\n"):
+    if len(lambda_name) == 0:
+      continue
     script_dir = "{0}/functions/{1}".format(os.getcwd(), lambda_name)
     with ctx.cd(script_dir):
       if os.path.exists("{0}.zip".format(lambda_name)):
         ctx.run("mv {0}.zip {0}.zip.backup".format(lambda_name))
-    if not os.path.exists("{0}/venv".format(script_dir)):
+      venv_path = "{0}/venv".format(script_dir)
+      if os.path.exists(venv_path):
+        ctx.run("rm -rf {0}".format(venv_path))
       ctx.run("python3 -m venv venv")
-      ctx.run("source venv/bin/activate && pip install -r requirements.txt")
-    with ctx.cd("cd {0}/venv/lib/python3.6/site-packages".format(script_dir)):
-      ctx.run("zip -r9 {0}/{1}.zip *".format(script_dir, lambda_name))
-    with ctx.cd(script_dir):
-      ctx.run("zip -r9 {0}/{1}.zip {1}.py".format(script_dir, lambda_name))
+      with ctx.prefix("source venv/bin/activate"):
+        ctx.run("pip install -r requirements.txt")
+      ctx.run("zip -r9 {0}/{1}.zip {0}/venv/lib/python3.6/site-packages/*".format(script_dir, lambda_name))
+      ctx.run("zip -r9 {0}/{1}.zip {0}/{1}.py".format(script_dir, lambda_name))
+      ctx.run("terraform init")
+      ctx.run("terraform plan")
+
+
+
+@task
+def deploy(ctx):
+    deploy_lambdas(ctx)
